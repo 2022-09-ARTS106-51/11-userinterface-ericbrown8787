898c324dfc3b6a885aa649e3962d8261cdf23181
Eric Brown <90017825+ericbrown8787@users.noreply.github.com>
2022-11-28 01:05:03 -0500

    Add toggling slide menu button

--- Assets/RW/Scripts/UIManager.cs
+++ Assets/RW/Scripts/UIManager.cs
@@ -9,6 +9,7 @@ public class UIManager : MonoBehaviour
     public Animator startButton;
     public Animator settingsButton;
     public Animator dialog;
+    public Animator contentPanel;
 
     public void StartGame()
     {
@@ -27,5 +28,11 @@ public class UIManager : MonoBehaviour
         startButton.SetBool("isHidden", false);
         settingsButton.SetBool("isHidden", false);
         dialog.SetBool("isHidden", true);
+    }
+
+    public void ToggleMenu()
+    {
+        bool isHidden = contentPanel.GetBool("isHidden");
+        contentPanel.SetBool("isHidden", !isHidden);
     }
 }
